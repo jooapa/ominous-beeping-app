bf7ab8327225f2873bb56658b41e02941a3e54cd
jooapa <jooa.akonpelto@gmail.com>
2023-04-19 17:33:21 +0300

    optomize

--- ominous/www/js/index.js
+++ ominous/www/js/index.js
@@ -1,35 +1,40 @@
-time = '2s';
-intervalTime = time * 1000;
+let timeNum = 1;
+let time = timeNum + "s";
+let intervalTime = timeNum * 1000;
+var circles = document.getElementsByClassName('circle');
 
-function playAnimation() {
-    var circles = document.getElementsByClassName('circle');
-    for (var i = 0; i < circles.length; i++) {
-      var circle = circles[i];
-      circle.style.animationName = 'scale' + (i + 1);
-      circle.style.animationDuration = time;
-      circle.style.animationTimingFunction = 'cubic-bezier(.08,.82,.36,1.11)';
-      circle.style.animationPlayState = 'running';
-  
-      // Remove class after animation has ended
-      circle.addEventListener('animationend', function(event) {
-        event.target.classList.remove('play-animation');
-        event.target.style.animationName = '';
-        event.target.style.animationDuration = '';
-        event.target.style.animationTimingFunction = '';
-        event.target.style.animationPlayState = '';
-      }, { once: true }); // Use "once" option to remove the event listener after it has been triggered once
-    }
+function playAnimation(time) {
+  for (var i = 0; i < circles.length; i++) {
+    var circle = circles[i];
+    circle.style.animationName = 'scale' + (i + 1);
+    circle.style.animationDuration = time;
+    circle.style.animationTimingFunction = 'cubic-bezier(.08,.82,.36,1.11)';
+    circle.style.animationPlayState = 'running';
+    circle.style.animationIterationCount = 'infinite'; // Set animation iteration count to 'infinite'
+  }
+
+    // Remove class after animation has ended
+    // circle.addEventListener('animationend', function(event) {
+    //   event.target.classList.remove('play-animation');
+    //   event.target.style.animationName = '';
+    //   event.target.style.animationDuration = '';
+    //   event.target.style.animationTimingFunction = '';
+    //   event.target.style.animationPlayState = '';
 }
+
 
 window.onload = function() {
-    infiniteAnimation();
+  playAnimation(time);
 };
-function infiniteAnimation() {
-
-    setInterval(function() {
-        playAnimation();
-    }, intervalTime);
+function setTime(x) {
+  let timeNum = x;
+  let time = timeNum + "s";
+  for (var i = 0; i < circles.length; i++) {
+    var circle = circles[i];
+    circle.style.animationDuration = time;
+  }
 }
+
 
 document.getElementById("startButton").addEventListener("click", function() {
   var icon = this.querySelector("i");
@@ -43,3 +48,5 @@ document.getElementById("startButton").addEventListener("click", function() {
     icon.classList.add("play");
   }
 });
+
+
